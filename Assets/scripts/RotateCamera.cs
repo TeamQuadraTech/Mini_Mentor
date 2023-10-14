0df828036cd0576e46f82c249d312011f9338511
mayank-always <140394120+mayank-always@users.noreply.github.com>
2023-10-14 09:09:32 -0400

    Changed Camera angle and added the podium back for a more decent look.

---
+++ Assets/scripts/RotateCamera.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player; // Reference to the player's transform
+    public Vector3 offset = new Vector3(0f, 1.5f, -3f); // Offset to position the camera relative to the player
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null)
+        {
+            Debug.LogWarning("Player transform not assigned!");
+            return;
+        }
+
+        // Calculate the desired position based on player's position and offset
+        Vector3 desiredPosition = player.position + offset;
+
+        // Smoothly move the camera to the desired position
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 5f);
+
+        // Make the camera look at the player
+        transform.LookAt(player.position);
+    }
+}
